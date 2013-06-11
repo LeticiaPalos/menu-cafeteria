c768650b78d0b592d6331ca5ea52be8c9ec496bd
LeticiaPalos <leticia.palos@tecvirtual.mx>
2013-06-11 15:58:53 -0400

    Update read_menu.js

--- read_menu.js
+++ read_menu.js
@@ -1,34 +1,45 @@
 $(document).ready(function(){
-	$(function listBooks(){
-		var d = new Date();
-		var month = d.getMonth() + 1;
-		var day = d.getDate();
-		var year = d.getFullYear();
-		var today = "\'" + (day<10?'0':'')+ day + '/' +(month<10?'0':'')+ month + '/' + year;
-		$.getJSON( "https://spreadsheets.google.com/feeds/list/0Ai8QKWdXy2RKdDZNeFBIZ0xiaFhtYkZoRGhMZ21CeGc/od6/public/values?alt=json-in-script&callback=?",
-		function (data) {
-			$.each(data.feed.entry, function(i,entry) {
-				var date = entry.gsx$fecha.$t;
-				if(today == date){
-					var barra = entry.gsx$barra.$t;
-					var item = entry.gsx$comida.$t.split(",");
-					if(barra == "Normal"){
+	$('a').click(function(){
+		//Asigna nombre de la cafetería
+		$('#cafeteria').text($(this).text());					
+	});
+	//Obtención y asignación de fecha
+	var d = new Date();
+	var month = d.getMonth() + 1;
+	var day = d.getDate();
+	var year = d.getFullYear();
+	var today = (day<10?'0':'')+ day + '/' +(month<10?'0':'')+ month + '/' + year;
+	$('#date').text(today);
+	today = "\'" + today;
+		
+	//Lectura de spreadsheet
+	$.getJSON( "https://spreadsheets.google.com/feeds/list/0Ai8QKWdXy2RKdDZNeFBIZ0xiaFhtYkZoRGhMZ21CeGc/od6/public/values?alt=json-in-script&callback=?",
+	function (data) {
+		$.each(data.feed.entry, function(i,entry) {
+			var date = entry.gsx$fecha.$t;
+			if(today == date){
+				var barra = entry.gsx$barra.$t;
+				var item = entry.gsx$comida.$t.split(",");
+				var menu = "";
+				if(barra == "Normal"){
+					$.each(item, function(i){
+						menu += item[i] + "<br>" ;
+					});
+					$('#normal').append(menu);
+				}else{
+					if(barra == "Bienestar"){
 						$.each(item, function(i){
-							$('#normal').append(item[i]+"<br>");
+							menu += item[i] + "<br>" ;
 						});
+						$('#bienestar').append(menu);
 					}else{
-						if(barra == "Bienestar"){
-							$.each(item, function(i){
-								$('#bienestar').append(item[i]+"<br>");
-							});
-						}else{
-							$.each(item, function(i){
-								$('#express').append(item[i]+"<br>");
-							});
-						}
+						$.each(item, function(i){
+							menu += item[i] + "<br>" ;
+						});
+						$('#express').append(menu);
 					}
 				}
-			});
+			}
 		});
 	});
 });
